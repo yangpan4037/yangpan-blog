d247b7b70e42f870b700253b0607727595b6aa52
杨潘 <916780135@qq.com>
2017-07-29 23:34:00 +0800

    yangpan

--- yangpan-core/src/main/java/site/yangpan/core/Application.java
+++ yangpan-core/src/main/java/site/yangpan/core/Application.java
@@ -1,28 +1,18 @@
-package site.yangpan.front;
+package site.yangpan.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import site.yangpan.core.dao.CityService;
 
 @Controller
 @SpringBootApplication //@SpringBootApplication注解相当于使用@Configuration，@EnableAutoConfiguration和@ComponentScan和他们的默认属性
-public class Application{
+public class Application {
 
-    @Autowired
-    private CityService cityService;
-
-    @GetMapping("/")
-    @ResponseBody
-    @Transactional(readOnly = true)
-    public String helloWorld() {
-        return this.cityService.getCity("Bath", "UK").getName();
+    @RequestMapping("/")
+    ModelAndView home() {
+        return new ModelAndView("index");
     }
 
     public static void main(String[] args) throws Exception {
