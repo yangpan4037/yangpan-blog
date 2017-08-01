65e987cc5d3dc4e58a94e1437babbc5093454ccf
yangpan <916780135@qq.com>
2017-08-01 18:00:59 +0800

    yangpan

--- yangpan-core/src/main/java/site/yangpan/core/CoreApplication.java
+++ yangpan-core/src/main/java/site/yangpan/core/CoreApplication.java
@@ -1,14 +1,11 @@
 package site.yangpan.core;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.Controller;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class CoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
     }
 }
