d550259647daf6511ee945c024dffd7fbbb9917c
杨潘 <916780135@qq.com>
2017-08-13 23:42:39 +0800

    yangpan

---
+++ yangpan-static-page/static/plugs/yangpan-dialog/jquery-yangpan.js
@@ -0,0 +1,16 @@
+/**
+ * time	2017-05-12
+ * name	杨潘 yangpan
+ * tel	13541292110
+ * mail 916780135@qq.com
+ */
+;(function($){
+	$.extend({
+		yangpanAlert:function(){
+			$('.ysw9MaskTip-box').remove();
+		},
+		yangpanConfirm:function(){
+
+		}
+	});
+})(jQuery);
